78fb86470ce81318e78fd933a86107db71859439
Oleksandr Hiliazov <oleksandr.hiliazov@eleks.com>
2023-01-03 18:04:56 +0100

    Added templates

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -2,9 +2,7 @@ from django.db import models
 
 
 class Category(models.Model):
-
     name = models.CharField(max_length=250, db_index=True)
-
     slug = models.SlugField(max_length=250, unique=True)
 
     class Meta:
@@ -15,17 +13,18 @@ class Category(models.Model):
 
 
 class Product(models.Model):
+    category = models.ForeignKey(
+        Category,
+        related_name="products",
+        on_delete=models.CASCADE,
+        null=True,
+    )
 
     title = models.CharField(max_length=250)
-
     brand = models.CharField(max_length=250, default="un-branded")
-
     description = models.TextField(blank=True)
-
     slug = models.SlugField(max_length=250)
-
     price = models.DecimalField(max_digits=4, decimal_places=2)
-
     image = models.ImageField(upload_to="images/")
 
     class Meta:
