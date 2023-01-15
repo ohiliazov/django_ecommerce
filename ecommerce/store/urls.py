f338963fd4a37d44e6eb52850e447b7bc4599a15
Oleksandr Hiliazov <oleksandr.hiliazov@eleks.com>
2023-01-15 12:36:15 +0100

    Added cart

--- ecommerce/store/urls.py
+++ ecommerce/store/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.store, name="store"),
+    path("product/<slug:product_slug>", views.product_info, name="product-info"),
+    path("search/<slug:category_slug>", views.list_category, name="list-category"),
 ]
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,13 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-from .models import Category
+from .models import Category, Product
 
 
 def store(request):
-    return render(request, "store/store.html")
+    all_products = Product.objects.all()
+    context = {"all_products": all_products}
+    return render(request, "store/store.html", context)
 
 
 def categories(request):
     all_categories = Category.objects.all()
 
     return {"all_categories": all_categories}
+
+
+def list_category(request, category_slug: str):
+    category = get_object_or_404(Category, slug=category_slug)
+    products = Product.objects.filter(category=category)
+
+    return render(
+        request,
+        "store/list-category.html",
+        {"category": category, "products": products},
+    )
+
+
+def product_info(request, product_slug: str):
+    product = get_object_or_404(Product, slug=product_slug)
+    context = {"product": product}
+    return render(request, "store/product-info.html", context)
